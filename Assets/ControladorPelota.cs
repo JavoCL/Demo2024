5b9787c0771b0a887f52d8c30189c049511f83e7
JavoCL <javierconchaa@gmail.com>
2024-09-13 15:41:46 -0300

    Post Clase XII: Unity 3D Buenas practicas - Inicio Pinball Demo
    - Headers
    - Tooltips
    - Structs
    - Script Templates

--- Assets/ControladorPelota.cs
+++ Assets/ControladorPelota.cs
@@ -7,12 +7,22 @@ using UnityEngine.InputSystem;
 
 public class ControladorPelota : MonoBehaviour
 {
+    [Header("CONFIGURACIÓN INPUTACTIONS")]
     public InputActionReference inputMovimiento;
-    public Vector2 direccionMovimiento2D;
-    public Vector3 direccionMovimiento3D;
 
+    // [Header("VECTORES DE MOVIMIENTO")]
+    // public Vector2 direccionMovimiento2D;
+    // public Vector3 direccionMovimiento3D;
+
+    [Header("VECTORES DE MOVIMIENTO")]
+    public Vectores vectores;
+
+    [Header("MISCELANEO")]
+    [Tooltip("Esta fue una variable de ejemplo para la clase. No se utiliza")]
+    public string variableDeEjemplo;
+
+    [Header("CONFIGURACIÓN FÍSICA")]
     public float coeficienteFuerza = 0f;
-
     public Rigidbody rigidbody3D;
 
     // Start is called before the first frame update
@@ -20,15 +30,17 @@ public class ControladorPelota : MonoBehaviour
     {
         rigidbody3D = this.gameObject.GetComponent<Rigidbody>();
 
-        direccionMovimiento2D = new Vector2();
-        direccionMovimiento3D = new Vector3();
+        vectores = new Vectores();
+
+        vectores.direccionMovimiento2D = new Vector2();
+        vectores.direccionMovimiento3D = new Vector3();
     }
 
     // Update is called once per frame
     void Update()
     {
-        direccionMovimiento2D = inputMovimiento.action.ReadValue<Vector2>();
-        direccionMovimiento3D = ReorganizaVector2(direccionMovimiento2D);
+        vectores.direccionMovimiento2D = inputMovimiento.action.ReadValue<Vector2>();
+        vectores.direccionMovimiento3D = ReorganizaVector2(vectores.direccionMovimiento2D);
         // (1,0,1)
     }
 
@@ -38,7 +50,7 @@ public class ControladorPelota : MonoBehaviour
         // Vector de magnitud=1, pero que es constante en Z
         // rigidbody3D.AddForce((direccionMovimiento3D + Vector3.forward) * coeficienteFuerza);
         // vector de magnitud=1 apunta hacia adelante del objeto
-        rigidbody3D.AddForce((direccionMovimiento3D + this.transform.forward) * coeficienteFuerza * direccionMovimiento3D.magnitude);
+        rigidbody3D.AddForce((vectores.direccionMovimiento3D + this.transform.forward) * coeficienteFuerza * vectores.direccionMovimiento3D.magnitude);
         Debug.DrawLine(this.transform.position, this.transform.position + rigidbody3D.GetAccumulatedForce(), Color.red);
         Debug.DrawLine(this.transform.position, this.transform.position + rigidbody3D.velocity, Color.green);
     }
@@ -107,5 +119,16 @@ public class ControladorPelota : MonoBehaviour
         Vector3 newVector = new Vector3(vector2.x, 0f, vector2.y);
 
         return newVector;
+    }
+
+    [System.Serializable]
+    public struct Vectores
+    {
+        public Vector2 direccionMovimiento2D;
+        public Vector3 direccionMovimiento3D;
+
+        [Header("CONFIGURACIONES DE POSICIONAMIENTO DE LA PELOTA")] // Hipotetico, concepto no utilizado
+        // Tener un registro de las posiciones cada 1 segundo (hipotetico, no utilizado)
+        public List<Vector3> registroPosiciones;
     }
 }
